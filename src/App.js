d0e7ce1f10480c75251e0bc3ba718a3309746a70
Rahul Patel <onlinerahulpatel@gmail.com>
2025-01-01 17:40:21 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ProfileCard from "./Component/Profilecard/ProfileCard";
 
 function App() {
   return (
-    <div className="App">
-      <ProfileCard />
-    </div>
+    <Router basename="/PORTFOLIO"> {/* Replace 'your-repo-name' with the name of your GitHub repository */}
+      <Routes>
+        <Route path="/" element={<ProfileCard />} /> {/* Default Route */}
+        <Route path="*" element={<ProfileCard />} /> {/* Catch-all for invalid paths */}
+      </Routes>
+    </Router>
   );
 }
 
